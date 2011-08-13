3247272851ad8f603db804729ca86bb620ec8b93
Martin Provencher <mprovencher86@gmail.com>
2011-08-13 13:57:36 -0400

    First version of the stalker stuff

---
+++ lib/job.rb
@@ -0,0 +1,13 @@
+Mandy.job "Stalking Stalker" do
+  map_tasks 5
+  reduce_tasks 3
+
+  map do |line|
+    stalker, stalking, date = line.split ","
+    emit date, stalker
+  end
+
+  reduce do |date, stalkers|
+    emit(date, stalkers.size)
+  end
+end
